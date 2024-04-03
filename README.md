a63ed1ca145e220333447b658af2b5f908fd02f3
Daniel Dias <danielbpdias@gmail.com>
2024-04-03 16:33:09 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Context Propagation between Rails API instrumented with ddtrace and OTel SDK
+
+**Under construction**
+
+This repository shows an example on how to propagate context between ddtrace instrumented APIs and OTel SDK APIs.
+
---
+++ quick_start_api_datadog/config/initializers/datadog.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'ddtrace'
+
+Datadog.configure do |c|
+  c.service = ENV['SERVICE_NAME']
+  c.logger.level = ::Logger::ERROR
+
+  c.tracing.instrument :rails
+  c.tracing.instrument :http
+
+  # List of header formats that should be extracted
+  c.tracing.distributed_tracing.propagation_extract_style = [ 'tracecontext' ]
+  c.tracing.distributed_tracing.propagation_extract_first = true
+
+  # List of header formats that should be injected
+  c.tracing.distributed_tracing.propagation_inject_style = [ 'tracecontext' ]
+
+  c.tracing.distributed_tracing.propagation_style = [ 'tracecontext' ]
+
+  c.tracing.trace_id_128_bit_generation_enabled = true
+end
